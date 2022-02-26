ddde0da7b2443f3331d595c1eae855fd19b64a8f
Riya Modi <riyakothwala@gmail.com>
2022-02-26 16:41:58 -0500

    added the Customer class

---
+++ assignment2/src/main/java/edu/gmu/swe645/assignment2/Customer.java
@@ -0,0 +1,23 @@
+package edu.gmu.swe645.assignment2;
+
+public class Customer {
+
+	private String name;
+	private int rank;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+}
