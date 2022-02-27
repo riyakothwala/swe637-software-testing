2849eab2abd9764063538652d01235ef2f7354fc
Xavier Dsouza <xavidsouza54@gmail.com>
2022-02-27 15:18:23 -0500

    Ranking Service class
    (cherry picked from commit 2a4ced4a114029615bfd975193f9e732087a7562)

---
+++ RankingService.java
@@ -0,0 +1,21 @@
+package edu.gmu.swe645.assignment2;
+import java.util.HashMap;
+
+public class RankingService{
+    public Rank getRank(Customer customer){
+
+
+        HashMap<String, Integer> rank = new HashMap<>();
+
+        rank.put("Riya", 1);
+        rank.put("Daksha", 2);
+        rank.put("Barid", 3);
+        rank.put("Xavier", 4);
+
+
+        Rank rankobj = new Rank(rank);
+
+        return rankobj;
+
+    }
+}
