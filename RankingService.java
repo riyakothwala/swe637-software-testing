807cf7f51d85e72a31a5ad60d7521a60a26b5d22
Xavier Dsouza <xavidsouza54@gmail.com>
2022-02-27 15:40:32 -0500

    check line 1
    (cherry picked from commit 5c027420b8da453474e84f156b8f71de091b0295)

--- RankingService.java
+++ RankingService.java
@@ -3,10 +3,7 @@ import java.util.HashMap;
 
 public class RankingService{
     public HashMap<String, Integer> getRank(Customer customer){
-
-
         HashMap<String, Integer> rank = new HashMap<>();
-
         rank.put("Riya", 1);
         rank.put("Daksha", 2);
         rank.put("Barid", 3);
