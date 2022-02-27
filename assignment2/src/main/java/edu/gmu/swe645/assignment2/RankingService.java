8132afb03b7efc26de8e464b20f6e3b94efe4895
Xavier Dsouza <xavidsouza54@gmail.com>
2022-02-27 15:18:23 -0500

    Moved ranking service class to assignment2 project.

--- assignment2/src/main/java/edu/gmu/swe645/assignment2/RankingService.java
+++ assignment2/src/main/java/edu/gmu/swe645/assignment2/RankingService.java
@@ -1,5 +1,16 @@
 package edu.gmu.swe645.assignment2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RankingService {
 
+	public Map<String, Integer> getRank(Customer customer) {
+		Map<String, Integer> rank = new HashMap<>();
+		rank.put("Riya", 1);
+		rank.put("Daksha", 2);
+		rank.put("Barid", 3);
+		rank.put("Xavier", 4);
+		return rank;
+	}
 }
