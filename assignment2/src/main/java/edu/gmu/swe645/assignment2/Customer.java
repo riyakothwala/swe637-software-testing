b9de6ac703564279a23c7d10b711c23623568ca2
Riya Modi <riyakothwala@gmail.com>
2022-02-27 14:47:18 -0500

    update the Customer class and created the Rank class

--- assignment2/src/main/java/edu/gmu/swe645/assignment2/Customer.java
+++ assignment2/src/main/java/edu/gmu/swe645/assignment2/Customer.java
@@ -3,21 +3,13 @@ package edu.gmu.swe645.assignment2;
 public class Customer {
 
 	private String name;
-	private int rank;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+	public Customer(String name) {
+		super();
 		this.name = name;
 	}
 
-	public int getRank() {
-		return rank;
-	}
-
-	public void setRank(int rank) {
-		this.rank = rank;
+	public String getName() {
+		return name;
 	}
 }
---
+++ assignment2/src/main/java/edu/gmu/swe645/assignment2/Rank.java
@@ -0,0 +1,15 @@
+package edu.gmu.swe645.assignment2;
+
+public class Rank {
+
+	int rank;
+
+	public Rank(int rank) {
+		super();
+		this.rank = rank;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+}
