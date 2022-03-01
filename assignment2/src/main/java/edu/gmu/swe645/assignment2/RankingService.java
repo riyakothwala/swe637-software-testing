699f1413a001da8b80ca72eb055cd507292d4fbc
Xavier Dsouza <xavidsouza54@gmail.com>
2022-03-01 14:53:37 -0500

    Modified RankingService

--- assignment2/src/main/java/edu/gmu/swe645/assignment2/RankingService.java
+++ assignment2/src/main/java/edu/gmu/swe645/assignment2/RankingService.java
@@ -1,16 +1,20 @@
 package edu.gmu.swe645.assignment2;
+import java.util.Random;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RankingService {
 
-	public Map<String, Integer> getRank(Customer customer) {
-		Map<String, Integer> rank = new HashMap<>();
-		rank.put("Riya", 1);
-		rank.put("Daksha", 2);
-		rank.put("Barid", 3);
-		rank.put("Xavier", 4);
-		return rank;
+	public Rank getRank(Customer customer) {
+		Random rand = new Random();
+
+		int customer_rank = rand.nextInt(50);
+
+		Rank rankObj = new Rank(customer_rank);
+
+		return rankObj;
 	}
 }
+
+
