71524481165698002778533b4e14b414f0dc28d0
Riya Modi <riyakothwala@gmail.com>
2022-03-22 09:23:37 -0400

    assignment 3 project setup

---
+++ assignment3/src/main/java/edu/gmu/swe645/assignment3/App.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe645.assignment3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ assignment3/src/test/java/edu/gmu/swe645/assignment3/CalTest.java
@@ -0,0 +1,16 @@
+package edu.gmu.swe645.assignment3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class CalTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
