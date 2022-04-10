a1f2c21e9ac13c595c7f8fe24da75215c646f1ff
BaridK <22103599+BaridK@users.noreply.github.com>
2022-04-10 14:13:29 -0400

    Added Linked List Class & junit test

---
+++ assignment5/src/assignment5/LinkedListTest.java
@@ -0,0 +1,38 @@
+package assignment5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest {
+	
+	@BeforeEach
+	void setup() {
+		System.out.println("Setting up test...");
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("Tearing down test...");
+	}
+	
+	
+	////Start of Barid's tests
+	@Test
+	void test() {
+		System.out.println("Starting Test 1");
+	}
+	////End of Barid's tests
+	
+	////Start of Daksha's tests
+	////End of Daksha's tests
+	
+	////Start of Xavier's tests
+	////End of Xavier's tests
+	
+	////Start of Riya's tests
+	////End of Riya's tests
+	
+}
