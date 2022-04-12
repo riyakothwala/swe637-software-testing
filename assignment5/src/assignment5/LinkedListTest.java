bc55b6ce6196f58f4c272a39b72ef6348b65efda
BaridK <22103599+BaridK@users.noreply.github.com>
2022-04-12 14:41:47 -0400

    Finished my part of coverage testing

--- assignment5/src/assignment5/LinkedListTest.java
+++ assignment5/src/assignment5/LinkedListTest.java
@@ -1,6 +1,9 @@
 package assignment5;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +17,6 @@ class LinkedListTest {
 	@BeforeEach
 	void setup() {
 		System.out.println("Setting up test...");
-		
 	}
 	
 	@AfterEach
@@ -27,11 +29,13 @@ class LinkedListTest {
 	
 	
 	////Start of Barid's tests
-	@Test
 	
-	//This test focuses on the following methods from LinkedList:
-	//LinkedList(), removeFirst(), contains(Object o), addAll(LinkedList<E> c)
-	//set(int index, E element), indexOf(), poll()
+	/* This test focuses on the following methods from LinkedList:
+	 * LinkedList(), removeFirst(), contains(Object o), addAll(LinkedList<E> c)
+	 * set(int index, E element), indexOf(), poll(), offerLast(), pollLast(),
+	 * removeLastOccurrence()
+	 */
+	@Test
 	void test1() {
 		System.out.println("Starting Test 1");
 		
@@ -76,6 +80,71 @@ class LinkedListTest {
 		//verify/test of poll() method
 		assertEquals("5", lLI.poll().toString());
 		
+		//call offerLast() method to add 6 to the end of Linked List
+		lLI.offerLast(6);
+		
+		//verify value at expected position
+		assertEquals(6,lLI.get(3));
+		
+		//call and verify pollLast() method
+		assertEquals(6,lLI.pollLast());
+		
+		//call and verify outcome of removeLastOccurrence() method
+		assertEquals(true,lLI.removeLastOccurrence(5));
+	}
+	
+	/* This test focuses on alternative branches for the following methods from LinkedList:
+	 * remove(), contains(), indexof(), poll(), pollLast(), removeLastOccurrence()
+	 */
+	@Test
+	void test2() {
+		System.out.println("Starting Test 2");
+		
+		//create new LinkedList object to test method LinkedList()
+		lLI = new LinkedList<Integer> ();
+		
+		//test and verify alternative branch of remove() method
+		Exception exception = assertThrows(NoSuchElementException.class, () -> {
+			lLI.remove();
+	    });
+		
+		lLI.add(1);
+		
+		//test and verify alternative branch of contains() method
+		assertEquals(false,lLI.contains(2));
+		
+		//test and verify alternative branch of indexOf() method
+		assertEquals(-1,lLI.indexOf(null));
+		
+		//add null to linked list
+		lLI.add(null);
+		
+		//for full coverage call indexOf() method with null already in list
+		assertEquals(1,lLI.indexOf(null));
+		
+		//reset list to an empty list
+		lLI = new LinkedList<Integer> ();
+		
+		//test and verify alternative branch of poll() method
+		assertEquals(null,lLI.poll());
+		
+		//test and verify alternative branch of pollLast() method
+		assertEquals(null,lLI.pollLast());
+		
+		lLI.add(1);
+		
+		//test and verify alternative branch of removeLastOccurrence() method
+		assertEquals(false,lLI.removeLastOccurrence(null));
+		
+		lLI.add(null);
+		
+		//test and verify alternative branch of removeLastOccurrence() method
+		assertEquals(true,lLI.removeLastOccurrence(null));
+		
+		//lastly, for full coverage of removeLastOccurrence() method test method where we look
+		//for element in list with elements where the element we are looking for dosen't
+		//exsist in list
+		assertEquals(false,lLI.removeLastOccurrence(2));
 	}
 	////End of Barid's tests
 	
